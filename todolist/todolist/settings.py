5e709a2cce036c839a9a9822a2fef2444688865f
Matthieu Felker <77665167+Matthieu-ui@users.noreply.github.com>
2022-03-25 18:20:56 -0700

    static

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -124,7 +124,7 @@ LOGIN_URL = 'login'
 2
 	
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
