7457aad50aad1a68d281e11825746c46d7de3e8d
Matthieu Felker <77665167+Matthieu-ui@users.noreply.github.com>
2022-03-25 18:02:11 -0700

    make it better

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -122,10 +122,7 @@ LOGIN_URL = 'login'
 2
 	
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
-)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
