b12fbac0146dccd06147d98b617986f55b7daa18
Matthieu Felker <77665167+Matthieu-ui@users.noreply.github.com>
2022-03-25 18:43:07 -0700

    task create test

--- todolist/base/tests.py
+++ todolist/base/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import Task
+from django.urls import reverse_lazy,reverse
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class TaskCreate(TestCase):
+    def test_TaskCreate(self):
+        data={
+            'Title':'testing',
+            'description':'fghdfghfd',
+            'complete':'',
+        
+        }
+
+
